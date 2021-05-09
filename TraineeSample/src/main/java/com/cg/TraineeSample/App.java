4c5c19b0160785454321b90ff686e4b8c3473928
Reshmi Reni <Reshmi Reni@Reshmi>
2021-05-09 12:12:32 +0530

    First commit -1

---
+++ TraineeSample/src/main/java/com/cg/TraineeSample/App.java
@@ -0,0 +1,14 @@
+package com.cg.TraineeSample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("SpringBook backendend app");
+    }
+}
